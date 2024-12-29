6bd060bb17047dd01d6a1c906be2605387191158
Nick Midmore <nmidmore@gmail.com>
2024-12-29 22:48:59 +0000

    added folder structure to projects and entity classes

---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseLibrary.Entities;
+public class Employee : BaseEntity
+{
+    public string? GovId { get; set; }
+    public string? FileNumber { get; set; }
+    public string? FullName { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Photo { get; set; }
+    public string? Other { get; set; }
+}
---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities;
+public class Relationship
+{
+    [JsonIgnore]
+    public List<Employee>? Employees { get; set; }
+}
