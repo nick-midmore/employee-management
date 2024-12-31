3a1f3deb983f02f6b276e3c63277a1a4b44ef451
Nick Midmore <nmidmore@gmail.com>
2024-12-31 10:20:58 +0000

    add CustomUserClaims dto and CustomAuthenticationStateProvider implementation

---
+++ BaseLibrary/DTOs/CustomUserClaims.cs
@@ -0,0 +1,2 @@
+﻿namespace BaseLibrary.DTOs;
+public record CustomUserClaims(string Id = null!, string Name = null!, string Email = null!, string Role = null!);
---
+++ ClientLibrary/Helpers/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,28 @@
+﻿using BaseLibrary.DTOs;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ClientLibrary.Helpers;
+public class CustomAuthenticationStateProvider(LocalStorageService localStorageService) : AuthenticationStateProvider
+{
+    private readonly ClaimsPrincipal anonymous = new(new ClaimsIdentity());
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var stringToken = await localStorageService.GetToken();
+        if (string.IsNullOrEmpty(stringToken)) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var deserialiseToken = Serialisations.DeserializeJsonString<UserSession>(stringToken);
+        if (deserialiseToken == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var getUserClaims = DecryptToken(deserialiseToken.Token!);
+        if (getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+        var claimsPrincipal = SetClaimPrincipal(getUserClaims);
+        return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+    }
+
+    private static CustomUserClaims DecryptToken(string jwtToken)
+    {
+        throw new NotImplementedException();
+    }
+}
